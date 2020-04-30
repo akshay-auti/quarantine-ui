12d16efda834d84bc485cb88c6e05bc872e1862a
akshay-auti <akshay.auti@neosofttech.com>
2020-04-30 15:06:39 +0530

    added next and previous button on table.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -34,24 +34,53 @@
         </q-card-section>
       </q-card>
     </div>
-    <div class="content">
-      <div class="display-download">
-      <download-csv :data="this.getCSVData">CSV 
-        <img src="~assets/download_icon.png" width="18%" height="18%"/>
-      </download-csv>
-    </div>  
-      <div class="results-left">
-      <q-table title="Assesments" 
-        :data="this.getTableData" 
-        :columns="columns" 
-        row-key="id"
-        :pagination.sync="pagination"
-        @request="onRequest" />
+    <div class="display-download">
+        <download-csv :data="this.getCSVData">
+          CSV
+          <img src="~assets/download_icon.png" width="18%" height="18%" />
+        </download-csv>
       </div>
-      <div class="results-right">
+    <div class="content">
+      <div class="results-left">
+        <table>
+          <thead>
+            <tr>
+              <th @click="sort('name')">Name</th>
+              <th>Phone</th>
+              <th>Risk</th>
+              <th>District</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr :key="data.id" v-for="data in this.getCSVData">
+              <td>{{data.name}}</td>
+              <td>{{data.phone}}</td>
+              <td>{{data.risk}}</td>
+              <td>{{data.district}}</td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- <q-table
+          title="Assesments"
+          :data="this.getCSVData"
+          :columns="columns"
+          row-key="id"
+          :pagination.sync="pagination"
+          @request="onRequest"
+        />-->
+      </div>
+      <!-- <div class="results-right"></div> -->
+    </div>
+    <div class="content">
+    <div id="outer">
+      <div class="inner">
+        <button v-on:click="this.onPrevious" :disabled="!this.getPreviousTableData">Previous</button>
+      </div>
+      <div class="inner">
+        <button v-on:click="this.onNext" :disabled="!this.getNextTableData">Next</button>
       </div>
     </div>
-
+    </div>
   </div>
   <!-- <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
     starting features will be giving them the ability to view the data basis:
@@ -81,55 +110,83 @@ export default {
       "getHighRisk",
       "getTableData",
       "getCSVData",
-      "getNextTableData"
+      "getNextTableData",
+      "getPreviousTableData"
     ])
+    // sortedCat: function() {
+    //   return this.getCSVData.sort((a, b) => {
+    //     let modifier = 1;
+    //     if (this.currentSortDir === "desc") modifier = -1;
+    //     if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+    //     if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+    //     return 0;
+    //   });
+    // }
   },
   data() {
     return {
+      currentSort: "name",
+      currentSortDir: "asc",
       pagination: {
         page: 1,
         rowsPerPage: 100,
         rowsNumber: 23000
       },
-      data: [],
-      columns: [
-        {
-          name: "name",
-          required: true,
-          label: "Name",
-          align: "left"
-        },
-        {
-          name: "phone",
-          align: "center",
-          label: "Phone",
-          field: "phone",
-          sortable: true
-        },
-        { name: "risk", label: "Risk", field: "risk", sortable: true },
-        { name: "district", label: "District", field: "district" },
-      ]
+      // data: [],
+      // columns: [
+      //   {
+      //     name: "name",
+      //     required: true,
+      //     label: "Name",
+      //     align: "left"
+      //   },
+      //   {
+      //     name: "phone",
+      //     align: "center",
+      //     label: "Phone",
+      //     field: "phone",
+      //     sortable: true
+      //   },
+      //   { name: "risk", label: "Risk", field: "risk", sortable: true },
+      //   { name: "district", label: "District", field: "district" }
+      // ]
     };
   },
   methods: {
     ...mapActions("covid", ["paginationSearch"]),
 
-    onRequest (props) {
-      const { page, rowsPerPage } = props.pagination
-      
-      // fetch data from "server"
-      const returnedData = this.paginationSearch(this.getNextTableData);
-      console.log(props.pagination.page)
+    // sort: function(s) {
+    //   //if s == current sort, reverse
+    //   if (s === this.currentSort) {
+    //     this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+    //   }
+    //   this.currentSort = s;
+    // },
 
-      // clear out existing data and add new
-      // this.data.splice(0, this.data.length, ...returnedData)
+    onNext: function() {
+      this.paginationSearch(this.getNextTableData);
+    },
 
-      // don't forget to update local pagination object
-      this.pagination.page = page
-      this.pagination.rowsPerPage = rowsPerPage
+    onPrevious: function() {
+      this.paginationSearch(this.getPreviousTableData);
     }
-},
-}
+
+    // onRequest(props) {
+    //   const { page, rowsPerPage } = props.pagination;
+
+    //   // fetch data from "server"
+    //   const returnedData = this.paginationSearch(this.getNextTableData);
+    //   console.log(props.pagination.page);
+
+    //   // clear out existing data and add new
+    //   // this.data.splice(0, this.data.length, ...returnedData)
+
+    //   // don't forget to update local pagination object
+    //   this.pagination.page = page;
+    //   this.pagination.rowsPerPage = rowsPerPage;
+    // }
+  }
+};
 </script>
 
 <style scoped>
@@ -188,4 +245,102 @@ export default {
   cursor: pointer;
 }
 
+/* table {
+  border-collapse: collapse;
+  overflow-x:auto;
+}
+
+table, th, td {
+  border: 1px solid black;
+}
+
+th, td {
+  padding: 15px;
+  text-align: left;
+} */
+
+@import url(http://fonts.googleapis.com/css?family=Open+Sans);
+body {
+  font-family: "Open Sans", sans-serif;
+}
+table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+table tr {
+  border: 1px solid #ddd;
+  padding: 5px;
+}
+table th,
+table td {
+  padding: 10px;
+  text-align: center;
+}
+table th {
+  font-size: 14px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+
+tr:hover {
+  background-color: #eee;
+}
+tr:nth-child(2n) {
+  background-color: #ddd;
+}
+tr:nth-child(2n):hover {
+  background-color: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
+  }
+  table thead {
+    display: none;
+  }
+  table tr {
+    border-bottom: 2px solid #ddd;
+    display: block;
+    margin-bottom: 10px;
+  }
+  table td {
+    border-bottom: 1px dotted #ccc;
+    display: block;
+    font-size: 13px;
+    text-align: right;
+  }
+  table td:last-child {
+    border-bottom: 0;
+  }
+  table td:before {
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+}
+button {
+  background-color: #1976d2; /* blue */
+  cursor: pointer;
+  /* border: none;
+  color: white;
+  padding: 5px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px; */
+}
+
+#outer {
+  width: 100%;
+  text-align:center;
+}
+.inner {
+  padding: 10px;
+  display: inline-block;
+}
 </style>
